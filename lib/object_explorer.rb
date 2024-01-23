5e33920d326c3ad7121a54a0ebb85c178ec482b3
Meikhail Major <meikhail@Meikhails-MacBook-Pro.local>
2024-01-23 15:08:41 -0700

    Added testing and revisions to be more complient with rubric

--- lib/object_explorer.rb
+++ lib/object_explorer.rb
@@ -1,18 +1,20 @@
 require 'colorize'
 require 'artii'
-require 'minitest/autorun'
+require 'minitest'
 
 
 class ObjectExplorer
 
   def manipulate_string(input_string)
+   @input_string = input_string
     puts "Reversed: #{input_string.reverse}"
-    reversed = input_string.reverse
+      reversed = input_string.reverse
     puts "Length: #{input_string.length}"
-    length = input_string.length
+      length = input_string.length
     puts "Uppercase: #{input_string.upcase}"
-    upcase = input_string.upcase
+      upcase = input_string.upcase
     return [reversed,length,upcase]
+    print [reversed,length,upcase]
   end
   
   # def manipulate_string(input_string)
@@ -23,20 +25,25 @@ class ObjectExplorer
   # end
 
   def manipulate_integer(input_integer)
+   @input_integer = input_integer
     puts "Squared: #{input_integer**2}"
-    squared = input_integer ** 2
+      squared = input_integer ** 2
     puts "Square Root: #{Math.sqrt(input_integer)}"
-    square_root = Math.sqrt(input_integer)
+      square_root = Math.sqrt(input_integer)
     return [squared,square_root]
   end
   
   def manipulate_array(input_array)
-  manipulated_array = input_array.split("")
+  #input_array.split("")
+  manipulated_array = input_array.split.map{ |n| n.chomp}
+  #squared_numbers = manipulated.map { |n| n**2 }
     print input_array.split("")
-    return manipulated_array
+    return "Your array: #{manipulated_array}"
+    puts "Your array: #{manipulated_array}"
   end
 
   def bonus_question(bonus)
+    bonus = bonus.strip.downcase
     if bonus == "module"
       puts "Correct!"
       return "Correct!"
@@ -45,4 +52,10 @@ class ObjectExplorer
       return "Incorrect! :("
     end
   end
+end
+
+exit = begin
+exit
+rescue SystemExit
+p 123
 end
