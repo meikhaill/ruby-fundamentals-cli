819de7b33ab8d0b66209c7e55e52cc16cdacdb1d
Meikhail Major <meikhail@Meikhails-MacBook-Pro.local>
2024-01-23 00:30:54 -0700

    added testing

--- lib/object_explorer.rb
+++ lib/object_explorer.rb
@@ -1,30 +1,48 @@
 require 'colorize'
 require 'artii'
+require 'minitest/autorun'
 
 
 class ObjectExplorer
-  
+
   def manipulate_string(input_string)
     puts "Reversed: #{input_string.reverse}"
+    reversed = input_string.reverse
     puts "Length: #{input_string.length}"
+    length = input_string.length
     puts "Uppercase: #{input_string.upcase}"
+    upcase = input_string.upcase
+    return [reversed,length,upcase]
   end
+  
+  # def manipulate_string(input_string)
+  #   puts "Reversed: #{input_string.reverse}"
+  #   puts "Length: #{input_string.length}"
+  #   puts "Uppercase: #{input_string.upcase}"
+  #   return "#{input_string.reverse}\n#{input_string.length}\n#{input_string.upcase}"
+  # end
 
   def manipulate_integer(input_integer)
     puts "Squared: #{input_integer**2}"
+    squared = input_integer ** 2
     puts "Square Root: #{Math.sqrt(input_integer)}"
-    puts "Doubled: #{input_integer*2}"
+    square_root = Math.sqrt(input_integer)
+    return [squared,square_root]
   end
   
   def manipulate_array(input_array)
+  manipulated_array = input_array.split("")
     print input_array.split("")
+    return manipulated_array
   end
 
   def bonus_question(bonus)
     if bonus == "module"
       puts "Correct!"
+      return "Correct!"
     else 
       puts "Incorrect! :("
+      return "Incorrect! :("
     end
   end
 end
--- test/object_explorer_test.rb
+++ test/object_explorer_test.rb
@@ -1,13 +1,32 @@
 
-require_relative './object_explorer'
+require_relative '../lib/object_explorer.rb'
 require 'minitest/autorun'
 require 'colorize'
 
+
+
 class TestObjectExplorer < Minitest::Test
-  def setup
-    @object_explorer = ObjectExplorer.new
+  def test_manipulate_string
+    object_explorer = ObjectExplorer.new
+      result = object_explorer.manipulate_string("Hello World")
+      assert_equal ["dlroW olleH",11,"HELLO WORLD"], result
   end
 
-  def test_string
+  def test_manipulate_integer
+    object_explorer = ObjectExplorer.new
+      result = object_explorer.manipulate_integer(9)
+      assert_equal [81,3], result
+  end
+
+  def test_manipulate_array
+    object_explorer = ObjectExplorer.new
+      result = object_explorer.manipulate_array("54321")
+      assert_equal ["5","4","3","2","1"], result
+  end
+
+  def test_bonus_question
+    object_explorer = ObjectExplorer.new
+      result = object_explorer.bonus_question("bruh")
+      assert_equal "Incorrect! :(", result
   end
 end
